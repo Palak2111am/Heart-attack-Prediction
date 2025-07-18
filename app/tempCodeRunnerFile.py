a2d6a87f9d77d34b662bcd3f79b5c8add411e5cb
Palak Patel <160333717+Palak2111am@users.noreply.github.com>
2025-07-18 14:47:31 +0530

    Add full backend and frontend

--- app/tempCodeRunnerFile.py
+++
@@ -1,9 +0,0 @@
-from dotenv import load_dotenv
-import os
-
-# Construct path to the .env file in the parent directory
-dotenv_path = os.path.join(os.path.dirname(__file__), '..', '.env')
-load_dotenv(dotenv_path)
-
-api_key = os.getenv("OPENAI_API_KEY")
-print("Loaded API Key:", api_key)
