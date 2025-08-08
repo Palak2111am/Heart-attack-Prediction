f029700cbc0d2442864dcffe27f5becec9a0b3d4
Palak Patel <160333717+Palak2111am@users.noreply.github.com>
2025-08-08 21:01:04 +0530

    Commit all recent changes

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -1,19 +1,10 @@
-import path from 'path';
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
   images: {
     unoptimized: true,
-  },
-  webpack(config) {
-    config.resolve.alias['@'] = path.resolve('./');
-    return config;
   },
 };
 
